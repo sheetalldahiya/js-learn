363e00b1edac531b210a7b39476988ad7f92405e
sheetalldahiya <169158869+sheetalldahiya@users.noreply.github.com>
2024-10-01 13:19:50 +0000

    hjk,

--- basics/02.js
+++ basics/02.js
@@ -35,15 +35,59 @@
 // }
 
 // let func = function(){
-//     console.log("hi");
+    // console.log("hi");
 // }
 
 // console.log(typeof arr); //object
 // console.log(typeof obj); //object
 // console.log(typeof func); //function
 
+//// stack->primitive  , heap->non-primitive
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
+// let name="sheetal";
+// let age=20;
+
+// //string interpolation
+// console.log(`name is ${name} and age is ${age}`);
+// console.log(name[1]);
+
+/////////////////////////////////////////
+
+// let str =new String("dahiya");
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.charAt(0));
+// console.log(str.indexOf('h'));  //-1 if not present
+// console.log(str.substring(1,3));
+// console.log(str.slice(-4,5));
+
+// let name="  sheetal  ";
+// console.log(name.trim());  // trimEnd() and trimStart()
+
+/////////////////////////////////////////
 
 
+// let url="www.xyz%20abc/sh%20dh";
+
+// console.log(url.replace('%20','-')); //replace only 1st occurance
+// console.log(url.replaceAll('%20','-')); //replace all occurances
+// console.log(url.replace(/%20/g,'-'));  //use g flag to indicate a global replacement   /pattern/flags
+
+// console.log(url.includes('xyz'));
+
+/////////////////////////////////////////
+
+
+let str="The quick brown fox jumps over the lazy dog";
+// console.log(str.split(" "));  
+// console.log(str.split(" ",3));            //separator,limit
+
+// console.log(str.charCodeAt(2));    //ascii value
+// console.log(str.endsWith('f'));
+// console.log(str.includes("fox")); 
+// console.log(str.lastIndexOf("a"));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
