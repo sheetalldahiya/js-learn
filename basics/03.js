e3b229c1f3b36bf4ad01ef04f7654d221acdcdfc
sheetalldahiya <169158869+sheetalldahiya@users.noreply.github.com>
2024-10-04 12:50:52 +0000

    new

---
+++ basics/03.js
@@ -0,0 +1,28 @@
+// const balance=new Number(21.456);
+// console.log(balance.toFixed(2));
+
+// console.log(balance.toPrecision(3));
+
+// const num=10000000;
+// console.log(num.toLocaleString());
+// console.log(num.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+// console.log(Math);
+// console.log(Math.round(3.72));
+// console.log(Math.pow(2,3));
+// console.log(Math.random());
+
+const min=15;
+const max=25;
+
+console.log( Math.floor(Math.random() * (max-min+1)) +min );  // including min and max
+console.log( Math.floor(Math.random() * (max-min)) +min );  // including min and excuding max
+
+
