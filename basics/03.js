6e86c125589a4c8ceb0bd450379826e672a9e427
sheetalldahiya <169158869+sheetalldahiya@users.noreply.github.com>
2024-10-04 13:30:59 +0000

    prec

--- basics/03.js
+++ basics/03.js
@@ -19,10 +19,33 @@
 // console.log(Math.pow(2,3));
 // console.log(Math.random());
 
-const min=15;
-const max=25;
+// const min=15;
+// const max=25;
 
-console.log( Math.floor(Math.random() * (max-min+1)) +min );  // including min and max
-console.log( Math.floor(Math.random() * (max-min)) +min );  // including min and excuding max
+// console.log( Math.floor(Math.random() * (max-min+1)) +min );  // including min and max
+// console.log( Math.floor(Math.random() * (max-min)) +min );  // including min and excuding max
 
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const mydate=new Date();
+// console.log(typeof mydate); //object
+
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+
+
+let newdate = new Date(2024,0,12);        //month start from 0
+console.log(newdate.toDateString());
+
+let newdate2 = new Date(2024,0,12,17,55,23);        
+console.log(newdate2.toLocaleString());
+
+let newdate3 = new Date("07-16-2020");        
+console.log(newdate3.toLocaleString());
